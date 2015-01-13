3a5a2ac3324f26265c5d6d60eb77bd58817b17b6
Tyler Garrett <tyler@tgrrtt.com>
2015-01-13 00:06:15 -0800

    Added some tests

---
+++ spec/calc.spec.js
@@ -0,0 +1,14 @@
+describe('calc', function() {
+  it('should be able to add', function() {
+    expect(complexNumberCalc('+', '1 + 2i','2 + 3i')).to.equal('3 + 5i');
+  });
+  it('should be able to subtract', function() {
+    expect(complexNumberCalc('-', '5 + 3i','2 + 1i')).to.equal('3 + 2i');
+  });
+  it('should be able to divide', function() {
+    expect(complexNumberCalc('/', '6 + 6i','2 + 1i')).to.equal('3 + 6i');
+  });
+  it('should be able to multiply', function() {
+    expect(complexNumberCalc('*', '2 + 2i','2 + 3i')).to.equal('4 + 6i');
+  });
+});
