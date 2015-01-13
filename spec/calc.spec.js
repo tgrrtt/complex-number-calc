dd6f0e48d0215bc89131d8a81efe44006486d9ab
Tyler Garrett <tyler@tgrrtt.com>
2015-01-13 00:08:53 -0800

    Added another subtraction test

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -4,6 +4,7 @@ describe('calc', function() {
   });
   it('should be able to subtract', function() {
     expect(complexNumberCalc('-', '5 + 3i','2 + 1i')).to.equal('3 + 2i');
+    expect(complexNumberCalc('-', '5 + 3i','7 + 4i')).to.equal('-2 + -1i');
   });
   it('should be able to divide', function() {
     expect(complexNumberCalc('/', '6 + 6i','2 + 1i')).to.equal('3 + 6i');
